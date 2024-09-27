41f615bece23443024f7e72543dfe99283489c89
Prerna Mudliar <prernamudliar@gmail.com>
2024-09-26 21:44:51 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setdata] = useState('');
+  
+  const [error, seterror] = useState(null);
+  
+  useEffect(
+    ()=>{
+      const fetch = new Promise(
+        (resolve,reject) =>{
+          setTimeout(() => {
+            resolve ("Command accepted")
+          }, 3000);
+        }
+      );
+      fetch.then(
+        (result)=>{
+          setdata(result)
+        }).catch(
+          (err)=>{
+            seterror(err)
+          }
+        )
+      
+    }
+  )
+  
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Promise inside useEffect</h2>
+      {error?<div>Error: {error} </div>: data? data:<div>Loading...</div>}
     </div>
   );
 }
